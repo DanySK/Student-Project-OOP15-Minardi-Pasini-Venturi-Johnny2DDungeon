0d4a3ca50b023848456df31d6fbae507f9fe99e7
Matteo Minardi <matteo.minardi@studio.unibo.it>
2016-02-11 16:43:46 +0100

    Item file removed

--- oop15-johnny2d/src/it/unibo/oop/model/Creature.java
+++ oop15-johnny2d/src/it/unibo/oop/model/Creature.java
@@ -1,7 +1,14 @@
 package it.unibo.oop.model;
-
+/**
+ * Class that rappresents the interface of a monster or creature that can be moved in the environment.
+ * @author Matteo Minardi
+ *
+ */
 public interface Creature extends Entity {
 
+	/**
+	 * Method that moves the creature basing the movement on the current input or controller decision.
+	 */
 	public void move();
 	
 	
