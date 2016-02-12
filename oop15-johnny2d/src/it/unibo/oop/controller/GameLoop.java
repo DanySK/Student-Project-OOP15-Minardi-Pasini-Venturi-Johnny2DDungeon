4d0be8d7aab97bc382a97bb8453c1eb54a3bd9f3
Paolo Venturi <paolo.venturi9@studio.unibo.it>
2016-02-12 17:22:26 +0100

    KeyManager improved.

--- oop15-johnny2d/src/it/unibo/oop/controller/GameLoop.java
+++ oop15-johnny2d/src/it/unibo/oop/controller/GameLoop.java
@@ -2,12 +2,9 @@ package it.unibo.oop.controller;
 
 import static it.unibo.oop.utilities.Direction.NONE;
 
-import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
-
 import it.unibo.oop.utilities.Direction;
 import it.unibo.oop.utilities.KeysManager;
+import it.unibo.oop.utilities.Pair;
 import it.unibo.oop.view.Launcher;
 import it.unibo.oop.view.Level;
 import it.unibo.oop.view.OptionsMenu;
@@ -17,25 +14,25 @@ import it.unibo.oop.view.Showable;
  * 
  * @author Paolo
  *
- *  class 
+ *  class implementing the Controller of the MVC model.
  */
 public class GameLoop implements Controller, StateObserver {
 
-    private final static int FRAMES_PER_SECOND = 60;
-    private final static int SLEEPING_TIME = 1/FRAMES_PER_SECOND;
+    private final static double FPS = 10;
+    private final static int TO_SECONDS = 1000;
+    private final static int SLEEPING_TIME = (int)(1/FPS * TO_SECONDS);
     private final Showable launcher;
     private final Showable options;
     private final Level level; /* da associarvi un'interfaccia */
-    
-    private volatile Direction pgDir = NONE;
-    private volatile boolean pgIsShooting = false;
+    private volatile Pair<Direction, Direction> pgDir;
+    private volatile boolean pgIsShooting;
     private final KeysManager<Direction> keysMan;
     
     public GameLoop() {
         this.launcher = new Launcher(this);
-        this.level = new Level(this.keysMan);
         this.options = new OptionsMenu();
         this.keysMan = new KeysManager<>();
+        this.level = new Level(this.keysMan);
     }
     
     @Override
@@ -44,6 +41,30 @@ public class GameLoop implements Controller, StateObserver {
         this.doLoop();
     }
     
+    /* GAME LOOP */
+    private void doLoop() {
+        while (true) {
+            try {
+                Thread.sleep(SLEEPING_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            /* valutare l'aggiunta di una wait per far eseguire eventuali eventi di key releasing (onde evitare che un tasto sia processato
+             * più volte in caso di un tasso di FPS elevato)
+             */
+            this.processKeys();
+            this.dbgKeysMan();        
+            /* chiamo C passandogli la direzione del pg e l'azione (spara o no) */
+            /* chiamo V e gli faccio disegnare il frame */
+        }
+    }
+    
+    private void processKeys() {
+        this.pgIsShooting = this.keysMan.isAKeyPressed(KeyCommands.SPACE);
+        this.pgDir = this.keysMan.getDirection();       
+    }
+    
+    /* valutare incapsulamento della seguente logica in componente ad hoc */
     @Override 
     public void stateAction(final State state) {
         switch (state) {
@@ -60,37 +81,14 @@ public class GameLoop implements Controller, StateObserver {
         }
     }
     
-    private void processKeys() {
-        synchronized(this) { /* per proteggere pgDir e pdIsShooting */
-            this.pgIsShooting = this.keysMan.isAKeyPressed(KeyEvent.VK_SPACE);
-            if (this.pgDir == NONE) {
-                this.pgDir = this.keysMan.getDirection();
-            }
+    private void dbgKeysMan() {
+        if (this.pgDir.getX() != NONE || this.pgDir.getY() != NONE) {
+            System.out.println("Dir 1: " + this.pgDir.getX() + " Dir 2: " + this.pgDir.getY());
         }
-    }
-
-    /* GAME LOOP */
-    private void doLoop() {
-        while (true) {
-            try {
-                Thread.sleep(SLEEPING_TIME);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            
-            synchronized(this) {    /* per proteggere pgDir e pdIsShoting */
-              //  System.out.println(this.keys);
-                this.processKeys();
-                /* chiamo C passandogli la direzione del pg e l'azione (spara o no) */
-                this.pgDir = Direction.NONE; /* dopo l'utilizzo reinizializzo tutto */
-                this.pgIsShooting = false;
-            }
-            /* chiamo V e gli faccio disegnare il frame */
-        }
+        System.out.println(this.pgIsShooting ? "SHOOT!" : "");
     }
     
     public static void main(String[] args) {
         new GameLoop().start();
-
     }
 }
--- oop15-johnny2d/src/it/unibo/oop/controller/KeyCommands.java
+++ oop15-johnny2d/src/it/unibo/oop/controller/KeyCommands.java
@@ -4,19 +4,37 @@ import static it.unibo.oop.utilities.Direction.*;
 import it.unibo.oop.utilities.Direction;
 import java.awt.event.KeyEvent;
 
+/**
+ * 
+ * @author Paolo
+ *
+ * Enum defining which keys can be pressed during the gameplay.
+ */
 public enum KeyCommands {
     W(KeyEvent.VK_W, UP),
     A(KeyEvent.VK_A, LEFT),
     S(KeyEvent.VK_S, DOWN),
     D(KeyEvent.VK_D, RIGHT),
     SPACE(KeyEvent.VK_SPACE, NONE),
-    ESC(KeyEvent.VK_ESCAPE, NONE);
+    ESC(KeyEvent.VK_ESCAPE, NONE); /* to pause the game */
     
-    private final int VK_Code;
+    private final int vkCode;
     private final Direction dir;
     
-    private KeyCommands(final int VK_Code, Direction dir) {
-        this.VK_Code = VK_Code;
+    private KeyCommands(final int vkCode, Direction dir) {
+        this.vkCode = vkCode;
         this.dir = dir;
+    }
+    
+    public Integer getVkCode() {
+        return this.vkCode;
+    }
+    
+    public Direction getDir() {
+        return this.dir;
+    }
+    
+    public boolean isMovement() {
+        return this.dir != NONE;
     }
 }
