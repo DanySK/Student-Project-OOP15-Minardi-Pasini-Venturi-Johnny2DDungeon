9bcbbb24649ac43d06c4671b2333b2787c5f6c90
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2016-02-17 16:14:37 +0100

    added class BackgroundPanel

--- oop15-johnny2d/src/it/unibo/oop/view/ImagePanel.java
+++ oop15-johnny2d/src/it/unibo/oop/view/ImagePanel.java
@@ -9,28 +9,18 @@ import javax.swing.JPanel;
 public class ImagePanel extends JPanel {
     
 	private static final long serialVersionUID = -992912248972775131L;
-	private Image image;  
-    private int iWidth2;
-    private int iHeight2;
+	private Image image;
 
-    public ImagePanel(Image image)
-    {
+    public ImagePanel(Image image) {
         this.image = Objects.requireNonNull(image);
-        this.iWidth2 = image.getWidth(this)/2;
-        this.iHeight2 = image.getHeight(this)/2;
     }
 
-
-    public void paintComponent(Graphics g)
-    {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         
         /* 
-         * i calcoli sotto servono per centrare l'immagine e farla 
-         * rimanere centrata anche con ridimensionamenti della finesta
+         * l'immagine viene adattata allo schermo.
          */
-        int x = this.getParent().getWidth()/2 - this.iWidth2;  
-        int y = this.getParent().getHeight()/2 - this.iHeight2;
-        g.drawImage(image, x, y, this);
+        g.drawImage(this.image, 0, 0, this.getWidth(), this.getHeight(), this);
     }
 }
