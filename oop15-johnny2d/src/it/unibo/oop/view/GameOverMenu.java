faafbe9d0db80f8539a543125727467049d111e6
Paolo Venturi <paolo.venturi9@studio.unibo.it>
2016-02-22 17:02:51 +0100

    some menus modified.

--- oop15-johnny2d/src/it/unibo/oop/view/GameOverMenu.java
+++ oop15-johnny2d/src/it/unibo/oop/view/GameOverMenu.java
@@ -1,12 +1,8 @@
 package it.unibo.oop.view;
 
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import it.unibo.oop.controller.AppState;
 import it.unibo.oop.controller.StateObserver;
@@ -19,8 +15,8 @@ public class GameOverMenu extends MenuPanel {
     
     private static final long serialVersionUID = 5096699286815303104L;
     private static final int FONT_SIZE = 30;
-    private static final int PANEL_WIDTH = 160;
-    private static final int PANEL_HEIGHT = 100;
+//    private static final int PANEL_WIDTH = 160;
+//    private static final int PANEL_HEIGHT = 100;
 
     /**
      * @param stateObs
@@ -37,11 +33,11 @@ public class GameOverMenu extends MenuPanel {
         label.setFont(new Font("AppStyle", Font.PLAIN, FONT_SIZE));
         label.setForeground(Color.WHITE);
         
-        final JPanel musicPane = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 10));
-        musicPane.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
-        musicPane.setOpaque(false);
-        musicPane.add(label);
-        this.addComponent(musicPane, false);
+//        final JPanel musicPane = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 10));
+//        musicPane.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+//        musicPane.setOpaque(false);
+//        musicPane.add(label);
+        this.addComponent(label, true);
         
         /* BUTTONS CREATION */
         this.addStateButton(new MenuPanel.StateButton("Replay", AppState.START), 
