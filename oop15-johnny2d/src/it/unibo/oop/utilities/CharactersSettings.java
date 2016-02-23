ec87b77dcc03a9f5bc965aa7dccb86978f99bcb3
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2016-02-23 18:13:25 +0100

    fixed bullets and coins size, hearts addedt to the view

--- oop15-johnny2d/src/it/unibo/oop/utilities/CharactersSettings.java
+++ oop15-johnny2d/src/it/unibo/oop/utilities/CharactersSettings.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 public enum CharactersSettings {
 	MAIN_CHARACTER(32, 48, new Velocity(8, 12, 5)),
 	BASIC_ENEMY(32, 48, new Velocity(5, 5, 10)),
-	BULLET(8, 8, new Velocity(20, 30, 1)),
-	BONUS(32, 32, null),
+	BULLET(16, 16, new Velocity(20, 30, 1)),
+	BONUS(16, 16, null),
 	WALL(32, 32, null);
 	
 	int width;
